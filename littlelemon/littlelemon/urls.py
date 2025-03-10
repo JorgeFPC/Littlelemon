06dbd8c419d52776505eb972dfbb915fa7d862de
Jorge Pat <a17003443@alumnos.uady.mx>
2025-03-09 21:30:22 -0600

    message

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.db import router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("restaurant/", include('restaurant.urls')),
+    path("admin/", admin.site.urls),
+    path('api/', include("restaurant.urls")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
